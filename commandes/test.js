32d02add421a4d8387b6eb9310581d217cf9217b
FEENIX-MD-V1 <164819368+feenixmd@users.noreply.github.com>
2024-03-25 08:45:41 +0530

    Create test.js

---
+++ commandes/test.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "repo", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello This is  *FEENIX-MD* \n\n ' + "The Following is *FEENIX-MD Repo.*";
+    let d = ' https://github.com/feenixmd/FEENIX-MD-V1';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/37d76d2eaa27a53eef207.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *FEENIX-MD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Sasiya*'
+      let varmess=z+d
+      var img='https://telegra.ph/file/13d63c21c1a665bfd8324.jpg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
